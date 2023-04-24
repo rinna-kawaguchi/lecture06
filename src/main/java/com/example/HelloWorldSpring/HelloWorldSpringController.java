9e9909d7394cb6f9d0dd1a7e928e5fb29e857c07
UserName <rin3810400h@gmail.com>
2023-04-24 18:03:15 +0900

    Helloをリクエストで受け取った国の言葉に変換

--- src/main/java/com/example/HelloWorldSpring/HelloWorldSpringController.java
+++ src/main/java/com/example/HelloWorldSpring/HelloWorldSpringController.java
@@ -1,12 +1,19 @@
 package com.example.HelloWorldSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldSpringController {
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    public String hello(@RequestParam(value = "country", defaultValue = " ") String country){
+        return switch (country) {
+            case "japan" -> country + "：こんにちは!";
+            case "us" -> country + "：Hello!";
+            case "france" -> country + "：Bonjour!";
+            case "korea" -> country + "：안녕하세요!";
+            default -> "こんにちは！";
+        };
     }
 }
